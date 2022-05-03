62c0dea4e6475abac97e82ecf312f12eaa00ca47
Kabir Ahuja <f2015791p@alumni.bits-pilani.ac.in>
2022-05-03 16:26:58 +0000

    Added almost all the code

--- src/fit_performance_functions.py
+++ src/fit_performance_functions.py
@@ -139,9 +139,7 @@ def fit_nd_eval_gpr_model_diff_test(train_df, test_df, lang, pivot_size):
     )
     gpr.fit(X_train, y_train)
 
-    X_test = test_df[
-        ["en_pivot_size", "en_tgt_trans_pivot_size", "tgt_pivot_size"]
-    ].values
+    X_test = test_df[["en_tgt_trans_pivot_size", "tgt_pivot_size"]].values
     y_test = test_df["f1_score"].values
 
     y_pred = gpr.predict(X_test)
@@ -151,8 +149,8 @@ def fit_nd_eval_gpr_model_diff_test(train_df, test_df, lang, pivot_size):
         {
             "Target Language": [lang for _ in range(len(X_test))],
             "English Pivot Size": [pivot_size for _ in range(len(X_test))],
-            "Translation Size": X_test[:, 1],
-            "Labelled Data Size": X_test[:, 2],
+            "Translation Size": X_test[:, 0],
+            "Labelled Data Size": X_test[:, 1],
             "F1-Score": y_test,
             "Predicted F1-Score": y_pred,
             "Absolute Errors": errors,
@@ -162,7 +160,7 @@ def fit_nd_eval_gpr_model_diff_test(train_df, test_df, lang, pivot_size):
     return gpr, pred_nd_error_df
 
 
-def fit_nd_eval_gpr_model(perf_df, lang):
+def fit_nd_eval_gpr_model(perf_df, lang, pivot_size):
 
     """
     Fits the training performance data with AMUE Performance Function
@@ -187,7 +185,7 @@ def fit_nd_eval_gpr_model(perf_df, lang):
         - pred_nd_error_df (pd.DataFrame) : Pandas dataframe containing the predictions and errors of the fitted model
     """
 
-    X = perf_df[["en_pivot_size", "en_tgt_trans_pivot_size", "tgt_pivot_size"]].values
+    X = perf_df[["en_tgt_trans_pivot_size", "tgt_pivot_size"]].values
     y = perf_df["f1_score"].values
     modes = perf_df["train_mode"].values
 
@@ -204,9 +202,9 @@ def fit_nd_eval_gpr_model(perf_df, lang):
         {
             "Train Mode": modes,
             "Target Language": [lang for _ in range(len(X))],
-            "English Pivot Size": X[:, 0],
-            "Translation Size": X[:, 1],
-            "Labelled Data Size": X[:, 2],
+            "English Pivot Size": [pivot_size for _ in range(len(X_test))],
+            "Translation Size": X[:, 0],
+            "Labelled Data Size": X[:, 1],
             "F1-Score": y,
             "Predicted F1-Score": y_pred,
             "Absolute Errors": errors,
